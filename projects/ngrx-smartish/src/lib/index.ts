1c666a358584947354d2c1ec4be0cf2b171ef047
Jon <jrista@users.noreply.github.com>
2021-03-11 09:14:50 -0700

    feat: dispatchable component actions (#33)
    * feat: dispatchable component actions
    
     + Add dispatchableFrom factory function to create directly dispatchable actions
     * Update public interface with new factory function
     * Update test project to prove new approach works
    
    * feat: dispatchable component actions
    
         + Add dispatcherFrom factory function to create action dispatcher functions
         * Update public interface with new factory function
         * Update test project to prove new approach works

--- projects/ngrx-smartish/src/lib/index.ts
+++ projects/ngrx-smartish/src/lib/index.ts
@@ -4,3 +4,5 @@ export { SMARTISH_STORE_TOKEN } from './token';
 export { NgRxSmartishModule } from './ngrx-smartish.module';
 export { NgRxSmartishComponent } from './ngrx-smartish-component';
 export { NgRxSmartishTestingModule } from './testing/ngrx-smartish-testing.module';
+export { dispatchableFrom } from './dispatch/dispatchableFrom.factory';
+export { dispatcherFrom } from './dispatch/dispatcherFrom.factory';
