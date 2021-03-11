8dfc8068056b9cbb325824fede7aa2ae9b8329d6
Jon Rista <jon.rista@briebug.com>
2021-03-10 22:29:57 -0700

    feat: dispatchable component actions
         + Add dispatcherFrom factory function to create action dispatcher functions
         * Update public interface with new factory function
         * Update test project to prove new approach works

--- projects/ngrx-smartish/src/lib/index.ts
+++ projects/ngrx-smartish/src/lib/index.ts
@@ -5,3 +5,4 @@ export { NgRxSmartishModule } from './ngrx-smartish.module';
 export { NgRxSmartishComponent } from './ngrx-smartish-component';
 export { NgRxSmartishTestingModule } from './testing/ngrx-smartish-testing.module';
 export { dispatchableFrom } from './dispatch/dispatchableFrom.factory';
+export { dispatcherFrom } from './dispatch/dispatcherFrom.factory';
--- src/app/taco/taco.component.ts
+++ src/app/taco/taco.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { createAction } from '@ngrx/store';
-import { NgRxSmartishComponent, dispatchableFrom } from '../../../projects/ngrx-smartish/src/lib';
+import { NgRxSmartishComponent, dispatchableFrom, dispatcherFrom } from '../../../projects/ngrx-smartish/src/lib';
 import { selectTaco } from '../store';
 
 const tacoAction = createAction(
@@ -17,5 +17,5 @@ export class TacoComponent extends NgRxSmartishComponent {
   selectors = { selectTaco };
   actions = { tacoAction };
 
-  action = dispatchableFrom(tacoAction);
+  tacoAction = dispatcherFrom(tacoAction);
 }
